cd29bab2eca91bae92099f473e368684e22001fd
kogami <felipemourao@univille.br>
2025-06-30 00:44:28 -0300

    up changes

--- app-a/src/main/java/br/univille/app_a/controller/HomeController.java
+++ app-a/src/main/java/br/univille/app_a/controller/HomeController.java
@@ -8,27 +8,46 @@ import io.dapr.client.domain.HttpExtension;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
 
-    //  ******** APPLICAÇÃO A  ************
+    //  ******** APPLICAÇÃO AAAAAAAAA  ************
 
     @GetMapping()
     public ResponseEntity index() {
         System.out.println("Hello from App A");
-
-        try(DaprClient daprClient = new DaprClientBuilder().build()) {
+        try(DaprClient daprClient = new DaprClientBuilder().build()){
+         
+            daprClient.invokeMethod("app-b", "/api", null, 
+                HttpExtension.GET).block();
             
-            daprClient.invokeMethod("app-b", "/api", null, HttpExtension.GET).block();
-
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("Error in Dapr Client");
+            // TODO: handle exception
         }
+
 
         return ResponseEntity.ok().body("Hello from App A");
     }
+
+    @PostMapping("/startaasync")
+    public ResponseEntity startAAsync(){
+        System.out.println("App A Started");
+
+        try(DaprClient daprClient = new DaprClientBuilder().build()){
+            var mensagem = "Hello from app A";
+
+            daprClient.publishEvent("pubsub-dapr","topicdapr", mensagem).block();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok().body("App A Started");
+    }
+
+
 }
